3e82e3179bf7e1a3305182870ee99693a1dcf876
rodgiee <rebecarodge@gmail.com>
2025-12-11 11:31:48 -0500

    added: form submit and log

--- aston-web/src/App.jsx
+++ aston-web/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
+import InputField from './components/inputField.jsx'
 import './App.css'
 
+function Print(formData){
+	const data = formData.values()
+	for(const value of data){console.log(value)}
+}
 function App() {
-  const [count, setCount] = useState(0)
+	//const [count, setCount] = useState(0)
 
-  return (
-    <>
-	<p>hello world!</p>
-    </>
-  )
+	return (
+		<>
+			<form action={Print}>
+				<InputField dataType="firstName" label="First Name"/>
+				<InputField dataType="lastName" label="Last Name"/>
+				<InputField dataType="phoneNumber" label="Phone Number"/>
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	)
 }
 
 export default App
